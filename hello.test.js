8cd66bcc95087026f7f0ab78a844b99c92dcc921
landyking <landykingdom@gmail.com>
2022-06-10 18:05:07 +0800

    initial commit

---
+++ hello.test.js
@@ -0,0 +1,12 @@
+var _ = require("lodash");
+jest.setTimeout(3600 * 1000);
+
+test("adds 1 + 2 to equal 3", () => {
+  console.log("xxxx 1111111111", new Date());
+  expect(1 + 2).toBe(3);
+});
+
+test("is nil", async () => {
+  let a;
+  expect(_.isNil(a)).toBe(true);
+});
